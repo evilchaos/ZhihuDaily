f9ff80fbbf829aa514fb4a204e6bad70af57abec
evilchaos <jcliubupt@163.com>
2016-07-19 18:09:39 +0800

    finish writting 知乎news_item

---
+++ app/src/main/java/com/example/liujiachao/zhihudaily/ZhihuActivity.java
@@ -0,0 +1,13 @@
+package com.example.liujiachao.zhihudaily;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZhihuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
