1c4872aeaf7b373744fd14ce22fffe66e44597fc
evilchaos <jcliubupt@163.com>
2016-07-20 15:47:15 +0800

    add zhihulistadapter

--- app/src/main/java/com/example/liujiachao/zhihudaily/ZhihuActivity.java
+++ app/src/main/java/com/example/liujiachao/zhihudaily/ZhihuActivity.java
@@ -1,13 +1,33 @@
 package com.example.liujiachao.zhihudaily;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
 public class ZhihuActivity extends AppCompatActivity {
+    public RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private ZhihuListAdapter zhihuListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initViews();
+
+    }
+
+    private void initViews() {
+        Context context = getBaseContext();
+        layoutManager = new LinearLayoutManager(context);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        zhihuListAdapter = new ZhihuListAdapter();
+        recyclerView.setAdapter(zhihuListAdapter);
+
+
 
     }
 }
---
+++ app/src/main/java/com/example/liujiachao/zhihudaily/ZhihuListAdapter.java
@@ -0,0 +1,28 @@
+package com.example.liujiachao.zhihudaily;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by liujiachao on 2016/7/20.
+ */
+public class ZhihuListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
