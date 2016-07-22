312a068eedd26fd994329e65b28c02c5e247fc10
evilchaos <jcliubupt@163.com>
2016-07-22 18:25:30 +0800

    finish creating realmObject

--- app/src/main/java/com/example/liujiachao/zhihudaily/ZhihuListAdapter.java
+++ app/src/main/java/com/example/liujiachao/zhihudaily/ZhihuListAdapter.java
@@ -4,6 +4,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bigkoo.convenientbanner.ConvenientBanner;
 
 /**
  * Created by liujiachao on 2016/7/20.
@@ -16,20 +20,23 @@ public class ZhihuListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     private static  int TYPE_DATE = 1;
     // view item
     private static int TYPE_ITEM = 2;
+    //footer item
+    private static int TYPE_FOOTER = 3;
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         if(viewType == TYPE_BANNER) {
-            View view = inflater.inflate(R.layout.zhihu_scroll_banner,null);
+            View view = inflater.inflate(R.layout.zhihu_scroll_banner,parent,false);
+            return new BannerViewHolder(view);
 
         } else if(viewType == TYPE_DATE) {
-            View view = inflater.inflate(R.layout.zhihu_date,null);
-
+            View view = inflater.inflate(R.layout.zhihu_date,parent,false);
+            return new DateViewHolder(view);
         } else if(viewType == TYPE_ITEM){
-
+            View view = inflater.inflate(R.layout.news_item,parent,false);
+            return new  ItemViewHolder(view);
         }
-        return null;
     }
 
     @Override
@@ -40,5 +47,44 @@ public class ZhihuListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class BannerViewHolder extends RecyclerView.ViewHolder {
+        public ConvenientBanner<ZhihuTop> banner;
+
+        public BannerViewHolder(View view) {
+            super(view);
+            banner = (ConvenientBanner) view.findViewById(R.id.convenientBanner);
+        }
+
+    }
+
+
+    public class DateViewHolder extends RecyclerView.ViewHolder {
+        public TextView DateText;
+        public DateViewHolder(View view) {
+            super(view);
+            DateText = (TextView)view.findViewById(R.id.zhihuDate);
+        }
+
+
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        public ItemViewHolder (View view) {
+            super(view);
+            header = (TextView)view.findViewById(R.id.news_time);
+            mImage =(ImageView)view.findViewById(R.id.news_img);
+            mTitle = (TextView)view.findViewById(R.id.news_title);
+            mItem = view.findViewById(R.id.news_item);
+
+        }
+        public TextView header;
+        public ImageView mImage;
+        public TextView mTitle;
+        public View mItem;
+        public ZhihuItemInfo zhihuItemInfo;
+
     }
 }
---
+++ app/src/main/java/com/example/liujiachao/zhihudaily/mvp/model/RealmString.java
@@ -0,0 +1,27 @@
+package com.example.liujiachao.zhihudaily.mvp.model;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by liujiachao on 2016/7/22.
+ */
+public class RealmString extends RealmObject {
+
+    private String val;
+    public RealmString() {
+
+    }
+
+    public RealmString(String val) {
+        this.val = val;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+}
