52c7c4a44b569f2ba30646287d15cca8802cbe37
evilchaos <jcliubupt@163.com>
2016-09-19 19:01:18 +0800

    fixing drawer can't display

--- app/src/main/java/com/example/liujiachao/zhihudaily/activity/ZhihuActivity.java
+++ app/src/main/java/com/example/liujiachao/zhihudaily/activity/ZhihuActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +21,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.example.liujiachao.zhihudaily.fragment.NavigationDrawerFragment;
 import com.example.liujiachao.zhihudaily.service.DownloadService;
 import com.example.liujiachao.zhihudaily.interfaces.MenuCallback;
 import com.example.liujiachao.zhihudaily.entity.MyTheme;
@@ -35,22 +38,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 //访问网络，加载消息数据，并将其保存到数据库， 在该activity的生命周期中完成
-public class ZhihuActivity extends AppCompatActivity implements ThemeDataView ,MenuCallback {
-
-    final static int HOME_WHIET = Color.WHITE;
-    final static int THEME_DATA_MSG = 1;
-
-    private ThemeDataPresenter  themeDataPresenter;
-    private RecyclerView swipe_rec_menu;
+public class ZhihuActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private boolean isTheme = false;
     private Toolbar toolbar;
-    private ThemeData themeData;
-    private ArrayList<MyTheme> myThemeList;
-    private RecMenuAdapter recMenuAdapter;
-    private TextView tv_home;
-    private Handler themeDataHandler;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,187 +53,57 @@ public class ZhihuActivity extends AppCompatActivity implements ThemeDataView ,M
     private void initViews() {
 
         setContentView(R.layout.main_activity);
-        toolbar =(Toolbar)findViewById(R.id.common_toolbar);
-        toolbar.setTitle("首页");
-        setSupportActionBar(toolbar);
+
+        setUpToolbar();
+
+        setUpDrawer();
+
+        setUpContentFragment();
+
+    }
+
+    private void setUpContentFragment() {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.content_fragment, new ZhihuHomeFragment());
         fragmentTransaction.commit();
-
-        swipe_rec_menu = (RecyclerView)findViewById(R.id.swipe_rec_menu);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
-
-        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name){
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-            }
-        };
-
-        drawerToggle.syncState();
-        drawerLayout.setDrawerListener(drawerToggle);
-        tv_home = (TextView)findViewById(R.id.tv_home);
-
-        recMenuAdapter = new RecMenuAdapter(this);
-        swipe_rec_menu.setAdapter(recMenuAdapter);
-
-        themeDataHandler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                switch (msg.what){
-                    case THEME_DATA_MSG :
-                        ArrayList<MyTheme> themeArrayList = (ArrayList<MyTheme>) msg.getData().getSerializable("theme_list");
-                        recMenuAdapter.updateData(themeArrayList);
-                        //recMenuAdapter.notifyDataSetChanged();
-                        break;
-                }
-                super.handleMessage(msg);
-            }
-        };
-        recMenuAdapter.setOnItemClickListener(new RecMenuAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                switch (view.getId()) {
-                    case R.id.linear_login:
-                        Intent login_intent = new Intent(ZhihuActivity.this, LoginActivity.class);
-                        startActivity(login_intent);
-                        break;
-
-                    case R.id.tv_collect:
-                        Intent collect_intent = new Intent(ZhihuActivity.this, LoginActivity.class);
-                        startActivity(collect_intent);
-                        break;
-
-                    case R.id.tv_download:
-                        Intent download_intent = new Intent(ZhihuActivity.this, DownloadService.class);
-                        startService(download_intent);
-                        break;
-
-                    case R.id.tv_home:
-                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                        ft.replace(R.id.content_fragment, new ZhihuHomeFragment());
-                        ft.commit();
-                        drawerLayout.setBackgroundColor(Color.parseColor("#f0f0f0"));
-                        drawerLayout.closeDrawers();
-                        getSupportActionBar().setTitle("首页");
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
-
-//        themeDataPresenter = new ThemeDataPresenter(this);
-//        themeDataPresenter.loadThemeData();
-    }
-
-    @Override
-
-    public void PassThemeDataToActivity(ThemeData themeData) {
-
-        int serial_num = 0;
-        ArrayList<MyTheme> myThemeList = new ArrayList<>();
-        this.themeData = themeData;
-        for (Theme item : themeData.getSubscribed() ) {
-            MyTheme myTheme = new MyTheme();
-            myTheme.setTheme(item);
-            myTheme.setSerial_num(serial_num);
-            myTheme.setSubscribed(true);
-            myTheme.setSelected(false);
-            myThemeList.add(myTheme);
-            serial_num++;
-        }
-
-        for (Theme item : themeData.getOthers()) {
-            MyTheme myTheme = new MyTheme();
-            myTheme.setTheme(item);
-            myTheme.setSerial_num(serial_num);
-            myTheme.setSubscribed(false);
-            myTheme.setSelected(false);
-            myThemeList.add(myTheme);
-            serial_num++;
-        }
-
-        this.myThemeList = myThemeList;
-        recMenuAdapter.updateData(myThemeList);
-        Message msg;
-        msg = Message.obtain();
-        msg.what = THEME_DATA_MSG;
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("theme_list", myThemeList);
-        msg.setData(bundle);
-        themeDataHandler.sendMessage(msg);
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_action_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
-                drawerLayout.closeDrawers();
-                return true;
-            } else if (isTheme) {
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.content_fragment,new ZhihuHomeFragment());
-                fragmentTransaction.commit();
-                isTheme = false;
-
-                for (MyTheme myTheme : myThemeList ) {
-                    myTheme.setSelected(false);
-                }
-
-                recMenuAdapter.notifyDataSetChanged();
-            }
-        }
-
-        return super.onKeyDown(keyCode, event);
     }
 
 
-    @Override
-    public void onRedrawMenu(int position) {
-        myThemeList.get(position - 1).setSubscribed(true);
-        Toast.makeText(ZhihuActivity.this,"无法真正订阅",Toast.LENGTH_SHORT).show();
-        recMenuAdapter.notifyDataSetChanged();//mark一下，此处要改，数据集顺序可能改变
-    }
-
-    @Override
-    public void onHandleEvent(int position) {
-        Theme theme = myThemeList.get(position - 1).getTheme();
-        ThemeFragment themeFragment = new ThemeFragment();
-        Bundle bundle = new Bundle();
-        bundle.putInt("theme_id", theme.getId());
-        themeFragment.setArguments(bundle);
-
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.content_fragment, themeFragment);
-        ft.commit();
-
-        getSupportActionBar().setTitle(theme.getName());
-        drawerLayout.closeDrawers();
-        tv_home.setBackgroundColor(HOME_WHIET);
-
-        myThemeList.get(position - 1).setSelected(true);
-        isTheme = true;
-        myThemeList.get(position - 1).setSelected(true);
-
-        for (MyTheme myTheme : myThemeList) {
-            boolean tmp = myTheme == myThemeList.get(position - 1);
-            myTheme.setSelected(tmp);
-        }
-
-        invalidateOptionsMenu();//菜单项已经改变，重新创造菜单
-        recMenuAdapter.notifyDataSetChanged();
-
+    private void setUpDrawer() {
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.nav_drawer_fragment);
+        DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        drawerFragment.setUpDrawer(R.id.nav_drawer_fragment, drawerLayout, toolbar);
 
     }
+
+    private void setUpToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.common_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("首页");
+        toolbar.inflateMenu(R.menu.toolbar_action_menu);
+    }
+
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if (keyCode == KeyEvent.KEYCODE_BACK) {
+//            if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+//                drawerLayout.closeDrawers();
+//                return true;
+//            } else if (isTheme) {
+//                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//                fragmentTransaction.replace(R.id.content_fragment,new ZhihuHomeFragment());
+//                fragmentTransaction.commit();
+//                isTheme = false;
+//
+//                for (MyTheme myTheme : myThemeList ) {
+//                    myTheme.setSelected(false);
+//                }
+//
+//                recMenuAdapter.notifyDataSetChanged();
+//            }
+//        }
+//
+//        return super.onKeyDown(keyCode, event);
+//    }
+
 }
--- app/src/main/java/com/example/liujiachao/zhihudaily/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/liujiachao/zhihudaily/fragment/NavigationDrawerFragment.java
@@ -1,16 +1,29 @@
 package com.example.liujiachao.zhihudaily.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.liujiachao.zhihudaily.R;
+import com.example.liujiachao.zhihudaily.activity.LoginActivity;
 import com.example.liujiachao.zhihudaily.adapter.RecMenuAdapter;
 import com.example.liujiachao.zhihudaily.entity.MyTheme;
 import com.example.liujiachao.zhihudaily.entity.Theme;
@@ -18,6 +31,7 @@ import com.example.liujiachao.zhihudaily.entity.ThemeData;
 import com.example.liujiachao.zhihudaily.interfaces.MenuCallback;
 import com.example.liujiachao.zhihudaily.mvp.presenter.ThemeDataPresenter;
 import com.example.liujiachao.zhihudaily.mvp.view.ThemeDataView;
+import com.example.liujiachao.zhihudaily.service.DownloadService;
 
 import java.util.ArrayList;
 
@@ -26,41 +40,183 @@ import java.util.ArrayList;
  */
 public class NavigationDrawerFragment extends Fragment implements ThemeDataView,MenuCallback {
 
+    final static int THEME_DATA_MSG = 1;
     private ThemeDataPresenter  themeDataPresenter;
-
+    private  RecMenuAdapter adapter;
+    private Handler themeDataHandler;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ArrayList<MyTheme> themeDatas;
+    private TextView mTvHome;
+    private FragmentActivity mActivity;
+    private boolean isTheme = false;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navigation_drawer,container,false);
-        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.drawerList);
-        RecMenuAdapter adapter =  new RecMenuAdapter(this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mActivity = getActivity();
+        mTvHome = (TextView)view.findViewById(R.id.tv_home);
 
 
-        themeDataPresenter = new ThemeDataPresenter(this);
-        themeDataPresenter.loadThemeData();
+        setUpRecyclerView(view);
+
+        receiveData();
 
         return view;
     }
 
+    private void setUpRecyclerView(View view) {
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.drawerList);
+        adapter =  new RecMenuAdapter(this);
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(new RecMenuAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                switch (view.getId()) {
+                    case R.id.linear_login:
+                        Intent login_intent = new Intent(mActivity, LoginActivity.class);
+                        startActivity(login_intent);
+                        break;
+
+                    case R.id.tv_collect:
+                        Intent collect_intent = new Intent(mActivity, LoginActivity.class);
+                        startActivity(collect_intent);
+                        break;
+
+                    case R.id.tv_download:
+                        Intent download_intent = new Intent(mActivity, DownloadService.class);
+                        getActivity().startService(download_intent);
+                        break;
+
+                    case R.id.tv_home:
+                        FragmentTransaction ft = mActivity.getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.content_fragment, new ZhihuHomeFragment());
+                        ft.commit();
+                        mDrawerLayout.setBackgroundColor(Color.parseColor("#f0f0f0"));
+                        mDrawerLayout.closeDrawers();
+                        ((AppCompatActivity)mActivity).getSupportActionBar().setTitle("首页");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    private void receiveData() {
+        themeDataHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what){
+                    case THEME_DATA_MSG :
+                        ArrayList<MyTheme> themeArrayList = (ArrayList<MyTheme>) msg.getData().getSerializable("theme_list");
+                        adapter.updateData(themeArrayList);
+                        break;
+                }
+                super.handleMessage(msg);
+            }
+        };
+
+        themeDataPresenter = new ThemeDataPresenter(this);
+        themeDataPresenter.loadThemeData();
+    }
+
+
+    public void setUpDrawer(int fragmentId,DrawerLayout drawerLayout,Toolbar toolbar) {
+        mDrawerLayout = drawerLayout;
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(),drawerLayout,toolbar,R.string.app_name, R.string.app_name) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToggle.syncState();
+            }
+        });
+
+    }
+
     @Override
     public void onRedrawMenu(int position) {
+        themeDatas.get(position - 1).setSubscribed(true);
+        Toast.makeText(getActivity(), "无法真正订阅", Toast.LENGTH_SHORT).show();
+        adapter.notifyDataSetChanged();//mark一下，此处要改，数据集顺序可能改变
 
     }
 
     @Override
     public void onHandleEvent(int position) {
+        Theme theme = themeDatas.get(position - 1).getTheme();
+        ThemeFragment themeFragment = new ThemeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("theme_id", theme.getId());
+        themeFragment.setArguments(bundle);
 
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_fragment, themeFragment);
+        ft.commit();
+
+
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(theme.getName());
+
+
+        mDrawerLayout.closeDrawers();
+        mTvHome.setBackgroundColor(Color.WHITE);
+
+        themeDatas.get(position - 1).setSelected(true);
+        isTheme = true;
+        for (MyTheme myTheme : themeDatas) {
+            boolean tmp = myTheme == themeDatas.get(position - 1);
+            myTheme.setSelected(tmp);
+        }
+        //invalidateOptionsMenu();//菜单项已经改变，重新创造菜单
+        adapter.updateData(themeDatas);
     }
 
     @Override
     public void PassThemeDataToActivity(ThemeData themeData) {
 
-        int serial_num = 0;
+        ArrayList<MyTheme> themeDatas = initThemeList(themeData);
+        initData(themeDatas);
+        sendMessage(themeDatas);
+    }
+
+    private void sendMessage(ArrayList<MyTheme> themeDatas) {
+        Message msg;
+        msg = Message.obtain();
+        msg.what = THEME_DATA_MSG;
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("theme_list", themeDatas);
+        msg.setData(bundle);
+        themeDataHandler.sendMessage(msg);
+    }
+
+    private void initData(ArrayList<MyTheme> themeDatas) {
+        this.themeDatas = themeDatas;
+    }
+
+    private ArrayList<MyTheme> initThemeList(ThemeData themeData) {
+
         ArrayList<MyTheme> myThemeList = new ArrayList<>();
-        this.themeData = themeData;
+        int serial_num = 0;
         for (Theme item : themeData.getSubscribed() ) {
             MyTheme myTheme = new MyTheme();
             myTheme.setTheme(item);
@@ -80,16 +236,6 @@ public class NavigationDrawerFragment extends Fragment implements ThemeDataView,
             myThemeList.add(myTheme);
             serial_num++;
         }
-
-        this.myThemeList = myThemeList;
-        recMenuAdapter.updateData(myThemeList);
-        Message msg;
-        msg = Message.obtain();
-        msg.what = THEME_DATA_MSG;
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("theme_list", myThemeList);
-        msg.setData(bundle);
-        themeDataHandler.sendMessage(msg);
-
+        return myThemeList;
     }
 }
